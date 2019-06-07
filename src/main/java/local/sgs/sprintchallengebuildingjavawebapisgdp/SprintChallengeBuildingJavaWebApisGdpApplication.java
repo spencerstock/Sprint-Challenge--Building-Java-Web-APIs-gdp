e3d1acbac8ce59ada40d20644e460c669a06bc40
Spencer Stock <spencerstock@gmail.com>
2019-06-07 09:12:37 -0600

    initial commit

---
+++ src/main/java/local/sgs/sprintchallengebuildingjavawebapisgdp/SprintChallengeBuildingJavaWebApisGdpApplication.java
@@ -0,0 +1,13 @@
+package local.sgs.sprintchallengebuildingjavawebapisgdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintChallengeBuildingJavaWebApisGdpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintChallengeBuildingJavaWebApisGdpApplication.class, args);
+    }
+
+}
