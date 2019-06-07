3a4069e709f997be36d2ef59c466316426b9efef
Spencer Stock <spencerstock@gmail.com>
2019-06-07 09:18:23 -0600

    initial commit

---
+++ src/main/kotlin/com/example/sprintchallengebuildingjavawebapisgdp/SprintChallengeBuildingJavaWebApisGdpApplication.kt
@@ -0,0 +1,11 @@
+package com.example.sprintchallengebuildingjavawebapisgdp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SprintChallengeBuildingJavaWebApisGdpApplication
+
+fun main(args: Array<String>) {
+    runApplication<SprintChallengeBuildingJavaWebApisGdpApplication>(*args)
+}
